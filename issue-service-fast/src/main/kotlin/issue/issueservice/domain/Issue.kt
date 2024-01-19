ad622c77e468b021584b7b65c5f4aaeda39272aa
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-01-19 14:00:39 +0900

    #6 - Entity class 생성 및 BaseEntity 생성
    Entity class 생성 및 BaseEntity 생성

---
+++ issue-service-fast/src/main/kotlin/issue/issueservice/domain/Issue.kt
@@ -0,0 +1,35 @@
+package issue.issueservice.domain
+
+import issue.issueservice.domain.enums.IssuePriority
+import issue.issueservice.domain.enums.IssueStatus
+import issue.issueservice.domain.enums.IssueType
+import jakarta.persistence.*
+
+@Entity
+@Table
+class Issue(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column
+    var userId: Long,
+
+    @Column
+    var summary: String,
+
+    @Column
+    var description: String,
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    var type: IssueType,
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    var priority: IssuePriority,
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: IssueStatus,
+) : BaseEntity()
