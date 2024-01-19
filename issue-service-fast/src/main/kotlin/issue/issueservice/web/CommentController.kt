abc0c739d33199556dd0294033dde4330908107f
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-01-19 16:26:06 +0900

    #20 - comment 삭제 API 작성
    comment 삭제 API 작성

--- issue-service-fast/src/main/kotlin/issue/issueservice/web/CommentController.kt
+++ issue-service-fast/src/main/kotlin/issue/issueservice/web/CommentController.kt
@@ -4,6 +4,7 @@ import issue.issueservice.config.AuthUser
 import issue.issueservice.model.CommentRequest
 import issue.issueservice.model.CommentResponse
 import issue.issueservice.service.CommentService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -28,4 +29,12 @@ class CommentController(
         @PathVariable id: Long,
         @RequestBody request: CommentRequest,
     ) = commentService.edit(id, authUser.userId, request)
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(
+        authUser: AuthUser,
+        @PathVariable issueId: Long,
+        @PathVariable id: Long,
+    ) = commentService.delete(issueId, id, authUser.userId)
 }
