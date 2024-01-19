8d6151140841468eb3ede1b80b4ee45515ce5501
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-01-19 14:16:49 +0900

    #6 - Issue 등록 로직 작성
    Issue 등록 로직 작성

---
+++ issue-service-fast/src/main/kotlin/issue/issueservice/service/IssueService.kt
@@ -0,0 +1,28 @@
+package issue.issueservice.service
+
+import issue.issueservice.domain.Issue
+import issue.issueservice.domain.IssueRepository
+import issue.issueservice.model.IssueRequest
+import issue.issueservice.model.IssueResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class IssueService(
+    private val issueRepository: IssueRepository,
+) {
+
+    @Transactional
+    fun create(userId: Long, request: IssueRequest): IssueResponse {
+        val issue = Issue(
+            summary = request.summary,
+            description = request.description,
+            userId = userId,
+            type = request.type,
+            priority = request.priority,
+            status = request.status,
+        )
+
+        return IssueResponse(issueRepository.save(issue))
+    }
+}
