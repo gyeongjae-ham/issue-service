9bfa7447c9744079b4ca6be4785251b6411776b0
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-01-29 13:42:00 +0900

    #30 - 로그인, 로그아웃 기능 구현
    로그인, 로그아웃 기능 구현

--- user-service/src/main/kotlin/com/fastcampus/userservice/service/UserService.kt
+++ user-service/src/main/kotlin/com/fastcampus/userservice/service/UserService.kt
@@ -1,19 +1,33 @@
 package com.fastcampus.userservice.service
 
+import com.fastcampus.userservice.config.JWTProperties
 import com.fastcampus.userservice.domain.entity.User
 import com.fastcampus.userservice.domain.repository.UserRepository
+import com.fastcampus.userservice.exception.PasswordNotMatchedException
 import com.fastcampus.userservice.exception.UserExistsException
+import com.fastcampus.userservice.exception.UserNotFoundException
+import com.fastcampus.userservice.model.SignInRequest
+import com.fastcampus.userservice.model.SignInResponse
 import com.fastcampus.userservice.model.SignUpRequest
 import com.fastcampus.userservice.utils.BCryptUtils
+import com.fastcampus.userservice.utils.JWTClaim
+import com.fastcampus.userservice.utils.JWTUtils
 import mu.KotlinLogging
 import org.springframework.stereotype.Service
+import java.time.Duration
 
 @Service
 class UserService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val jwtProperties: JWTProperties,
+    private val cacheManager: CoroutineCacheManager<User>,
 ) {
 
     val log = KotlinLogging.logger {}
+
+    companion object {
+        private val CACHE_TTL = Duration.ofMinutes(1)
+    }
 
     suspend fun signUp(signUpRequest: SignUpRequest) {
         with(signUpRequest) {
@@ -27,5 +41,34 @@ class UserService(
             )
             userRepository.save(user)
         }
+    }
+
+    suspend fun signIn(request: SignInRequest): SignInResponse {
+        return with(userRepository.findByEmail(request.email) ?: throw UserNotFoundException()) {
+            val verified = BCryptUtils.verify(request.password, password)
+            if (!verified) {
+                throw PasswordNotMatchedException()
+            }
+
+            val jwtClaim = JWTClaim(
+                userId = id!!,
+                email = email,
+                profileUrl = profileUrl,
+                username = username,
+            )
+
+            val token = JWTUtils.createToken(jwtClaim, jwtProperties)
+            cacheManager.awaitPut(key = token, value = this, ttl = CACHE_TTL)
+
+            SignInResponse(
+                email = email,
+                username = username,
+                token = token,
+            )
+        }
+    }
+
+    suspend fun logout(token: String) {
+        cacheManager.awaitEvict(token)
     }
 }
